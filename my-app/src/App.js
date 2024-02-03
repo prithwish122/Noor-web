3c5592d788cde48eea93620a1fb7b8caac56241c
prithwish122 <prithwishchatterjee1277@gmail.com>
2024-02-03 22:37:52 +0530

    minor changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
+// import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import './App.css';
 import Hero from './components/Hero';
 import Task from './components/Task';
@@ -10,17 +10,17 @@ function App() {
   return (
     <>
     <div className='bg'>
-      <Router>
+      {/* <Router> */}
         <Navbar/>
         <Hero/>
         <Task/>
         {/* <div className='About'> */}
           <About/>
         {/* </div> */}
-        <Routes>
-          <Route path='/' exact />
-        </Routes>
-      </Router>
+        {/* <Routes> */}
+          {/* <Route path='/' exact /> */}
+        {/* </Routes> */}
+      {/* </Router> */}
       </div>   
     </>
   );
--- my-app/src/components/Navbar.jsx
+++ my-app/src/components/Navbar.jsx
@@ -1,24 +1,44 @@
-import React,{useState} from "react";
+import React from "react";
+// import {Routes, Route , Switch} from 'react-router-dom';
 import {Link} from 'react-router-dom';
 import '../components/Navbar.css';
 import Logo from '../components/Logo.png';
+// import Profile from "./Profile";
+
 
 function Navbar() {
     return(
         
             <nav className="navbar">
-                    <a href="/" className="title">
-                    <img  class="title" src={Logo}/>
+                    <a href="/" className="title"> 
+                    {/* // eslint-disable-next-line */}
+
+                    <img  class="title" src={Logo}  alt="" />
                     </a>
                     <div className="menu">
                         <ul className="menu-items">
                             <li>
-                                <a href="#about">About</a>
+                            <a className="a" to="/">About</a>
                             </li>
                             <li>
-                                <a href="#profile">Profile</a>
+                            <a to="/Profile">Profile</a>
                             </li>
                         </ul>
+                        {/* <Switch> */}
+                        {/* <Routes> */}
+                                {/* <Route path="/"/> */}
+                                    {/* <About /> */}
+                                    
+                                
+                                {/* <Route path="/Profile"/> */}
+                                    {/* <Profile/> */}
+                                
+                        {/* </Routes>
+                        </Switch> */}
+
+
+                        
+
                     </div>
             </nav>
         
